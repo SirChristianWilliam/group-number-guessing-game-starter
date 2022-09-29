75ca9cc9acf30ac912fd44ed88cbc78dc5bd1591
adamantdude <adam.plee23@gmail.com>
2022-09-29 14:28:30 -0500

    attempt branch 2 ; add tremendous work to project following whiteboard flowchart and architecture

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,90 @@
-$(document).ready(handleReady);
+$(handleReady);
 
 function handleReady() {
   console.log("jquery is loaded!")
+  loadItems(); //On page load, show the empty information 
+  $('#myForm').on('submit',submitOn);
 }
+//Global variable objects
+const pastGuesses = [];
+
+let winner = {
+  player1: false,
+  player2: false
+}
+let highOrLow = {
+  player1: "low",
+  player2: "low"
+}
+//Global variable objects.
+
+//loadItems is our render
+function loadItems() {
+  $('#pastContainer').text('');
+  for(let x of pastGuesses) {
+    $('#pastContainer').append(`
+     ${x.player1}<br>
+    ${x.player2}<br>
+    `);
+
+    
+  }
+  $('#chris').empty();
+  $('#adam').empty();
+
+}
+
+function submitOn(evt) {
+  evt.preventDefault();
+
+  let guesses = {
+    player1: $('#chris').val(),
+    player2: $('#adam').val()
+  }
+
+  pastGuesses.push(guesses);
+  $('#container').empty();
+  $('#container').append ( `
+    Player 1 guess: ${guesses.player1} <br>
+    Player 2 guess: ${guesses.player2} <br>
+  `)
+
+  $.ajax({
+    url: './info',
+    method: 'POST',
+    data: guesses
+  })
+  .then((response) => {
+    console.log("sending guesses over", response);
+    useWinner();
+  })
+  .catch(err => {
+    console.log("POST /info error", err);
+  })
+}
+
+function useWinner() {
+  $.ajax({
+    url: './getInfo',
+    method: 'GET'
+  })
+  .then ((response) => {
+    winner = response;
+    if(winner.player1 == true || winner.player2 == true) {
+      loadItems();
+    } else {
+      setHighLow();
+      loadItems();
+    }
+  })
+ }
+
+ function setHighLow() {
+  $.ajax({
+    url: './getInfo',
+    method: 'GET'
+  })
+  .then((response) => {
+    highOrLow = response;
+  })
+ }
--- server/server.js
+++ server/server.js
@@ -2,8 +2,27 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
 const PORT = 5000;
+ // track player guesses
+let players = {
+  player1: 0,
+  player2: 0
+}
+let whoWon = {
+  player1: false,
+  player2: false
+}
+let HL = {
+  player1: "low",
+  player2: "low"
+}
 
+// track number of guesses made
+let guesses = {
+  totalGuesses: 0
+}
 
+// keep random number to be guessed
+let randO = randomGen(1, 25);
 
 // This must be added before GET & POST routes.
 app.use(bodyParser.urlencoded({extended:true}))
@@ -11,9 +30,52 @@ app.use(bodyParser.urlencoded({extended:true}))
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
 
-// GET & POST Routes go here
+app.get('/getInfo', (req,res) => {
+  console.log('IN /getInfo');
+  res.send(HL);
+})
 
+// POST START
+app.post('/info', (req, res) => {
+  console.log('IN FIRST POST', req.body);
+  console.log("THE RANDOM NUMBER IS",randO)
+  players.player1 = req.body.player1;
+  players.player2 = req.body.player2;
+  compareWinners();
+
+  res.sendStatus(201);
+})
+
+// POST END
+// ------ ROUTES ------
 
 app.listen(PORT, () => {
   console.log ('Server is running on port', PORT)
 })
+
+// FUNCTIONS
+
+// randomGen() randomly generate a number between 1-25
+function randomGen(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+// compare() compare guesses by players to random number
+function compareWinners() {
+  console.log("ARE WE IN COMPARE WINNERS")
+  if(players.player1 === randO) {
+    whoWon.player1 = true;
+  } else if(players.player2 === randO) {
+    whoWon.player2 = true;
+  } else {
+    HL.player1 = players.player1 > randO ? 'high' : 'low';
+    HL.player2 = players.player2 > randO ? 'high' : 'low';
+  }
+  }
+
+
+
+// reset() reset game state to new start
+function reset() {
+  // 
+}
